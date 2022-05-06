c9bacddf2ac2412896af3d08ac97337d7d93ce7e
moneeb <moneebalalfi@gmail.com>
2022-05-07 01:49:45 +0300

    feat: adding hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+import { HardhatUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-ethers";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.matic.today",
+    //   accounts: [process.env.pk]
+    // },
+    // polygon: {
+    //   url: "https://polygon-rpc.com/",
+    //   accounts: [process.env.pk]
+    // }
+  },
+};
+
+export default config;
